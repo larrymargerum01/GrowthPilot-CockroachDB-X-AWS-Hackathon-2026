8ba6aa123215756e6473c15a88efc583e3a447f1
Larry Margerum <larrymargerum1950@gmail.com>
2026-08-03 23:57:36 +0200

    feature: add embedding generation interface

--- backend/memory/embedding.py
+++ backend/memory/embedding.py
@@ -4,5 +4,14 @@ class BedrockEmbeddingService:
     using AWS Bedrock.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, client):
+        self.client = client
+
+    def generate_embedding(self, text: str):
+        """
+        Generate an embedding vector for the given text.
+        """
+
+        response = self.client.invoke_model(body = text)
+
+        return response
